7aab8d742a282e78306adf60675762abb38a94f1
Gmgge <157505853@qq.com>
2023-07-13 09:24:37 +0800

    设定数据增强概率参数

--- image_aug.py
+++ image_aug.py
@@ -4,18 +4,17 @@ import imgaug as ia
 import imgaug.augmenters as iaa
 from PIL import Image
 
-aug_seq = iaa.Sequential([
+sometimes = lambda aug: iaa.Sometimes(0.5, aug)
+
+aug_seq = sometimes(iaa.Sequential([
     # Strengthen or weaken the contrast in each image.
     iaa.LinearContrast((0.9, 1.1)),
     # Apply affine transformations to each image.
     # Scale/zoom them, translate/move them, rotate them and shear them.
-    iaa.Affine(
-        scale={"x": (0.9, 1.0), "y": (0.9, 1.0)},
-        rotate=(-180, 180),
-    )
-], random_order=True) # apply augmenters in random order
+    iaa.Affine(scale={"x": (0.9, 1.0), "y": (0.9, 1.0)},rotate=(-180, 180),)
+    ], random_order=True)) # apply augmenters in random order
 
-def image_aug(image_data:Image.Image):
+def aug_sequential(image_data:Image.Image):
     """
     调用imgaug进行图像增强，由于训练图像为PIL.Image.Image格式，存在一定转换
     """
@@ -27,5 +26,6 @@ def image_aug(image_data:Image.Image):
 if __name__ == "__main__":
     image_path = r"D:\py_project\seal\faker_seal_image\0.png"
     image_data = Image.open(image_path)
-    images_aug = image_aug(image_data)
-    images_aug.show()
+    for i in range(50):
+        images_aug = aug_sequential(image_data)
+        images_aug.show()
